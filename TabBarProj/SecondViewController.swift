8b581a3740332afe9fa70c347a3a9705127cbad2
1389028 <vaibhav.u1@tcs.com>
2020-07-28 17:31:30 +0530

    Initial Commit

---
+++ TabBarProj/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabBarProj
+//
+//  Created by Vaibhav U on 28/07/20.
+//  Copyright © 2020 Vaibhav U. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
